ce0093437cdb47fb1991c1217debabfeb646e0f1
Nicolas Thouchkaieff <nico.thouch94@gmail.com>
2022-10-08 15:13:14 +0200

    Upgrade to React 18 + add eslint TS rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 // .eslintrc.js
 module.exports = {
-    extends: ["next/core-web-vitals", "prettier"],
-    rules: {
-        "import/no-anonymous-default-export": "off",
-    },
+  extends: ['next/core-web-vitals', 'prettier', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    'import/no-anonymous-default-export': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
 };
